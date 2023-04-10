c8b95d783e7515269bae0dcb1a68868b88fc241e
Curtis Slone <curtisdslone@gmail.com>
2023-04-10 11:19:38 +0200

    Stash Changes

--- modules.keyvault.tf
+++ modules.keyvault.tf
@@ -1,18 +1,26 @@
 module "mod_state_keyvault" {
     source  = "azurenoops/overlays-key-vault/azurerm"
 
+    depends_on = [
+      module.mod_network_artifacts_rg,
+      module.mod_spn
+    ]
+
     create_key_vault_resource_group = false
     custom_resource_group_name = module.mod_network_artifacts_rg.resource_group_name
     location = var.location
     environment = var.environment
     workload_name = var.workload_name
-
     enabled_for_deployment = true
     enabled_for_disk_encryption = true
 
     public_network_access_enabled = false
     purge_protection_enabled = false # change to true for development
     enable_private_endpoint = false
+
+
+    
+    rbac_authorization_enabled = true
 
     admin_objects_ids = [
         data.azuread_client_config.current.object_id
