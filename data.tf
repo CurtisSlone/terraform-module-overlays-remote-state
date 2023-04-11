aa440cdff8d93192ae90c44cf8b6c8b1190d5361
Curtis Slone <curtisdslone@gmail.com>
2023-04-11 15:23:19 +0200

    KV module successful

--- data.tf
+++ data.tf
@@ -1,3 +1,11 @@
 data "azurerm_client_config" "current" {}
 
 data "azuread_client_config" "current" {}
+
+data "azuread_group" "KV_Readers" {
+  display_name = "KV-Readers"
+}
+
+data "azuread_group" "KV_Admins" {
+  display_name = "KV-Admins"
+}
--- modules.keyvault.tf
+++ modules.keyvault.tf
@@ -11,23 +11,21 @@ module "mod_state_keyvault" {
     location = var.location
     environment = var.environment
     workload_name = var.workload_name
-    enabled_for_deployment = true
-    enabled_for_disk_encryption = true
 
     public_network_access_enabled = false
     purge_protection_enabled = false # change to true for development
     enable_private_endpoint = false
-
+    
 
     
     rbac_authorization_enabled = true
 
     admin_objects_ids = [
-        data.azuread_client_config.current.object_id
+        data.azuread_group.KV_Admins.object_id
     ]
 
     reader_objects_ids = [
-        module.mod_spn.service_principal_object_id
+        data.azuread_group.KV_Readers.object_id
     ]
 
 
